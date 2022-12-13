baa67574119205dececd001858ec34f67bc33103
Akira Kozakai <akiraak@gmail.com>
2022-12-12 23:00:59 -0800

    Add ironic ai

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 if __name__ == '__main__':
-    prompt_text = """
-以下は人工知能アシスタントとの会話です。このアシスタントは丁寧で、創造的で、頭が良く、とてもフレンドリーです。
-
+    prompt_text = "以下は人工知能アシスタントとの会話です。このアシスタントは丁寧で、創造的で、頭が良く、とてもフレンドリーです。\n"
+    #prompt_text = "以下は人工知能アシスタントとの会話です。このアシスタントは皮肉たっぷりに答えてくれる渋いAIです。\n\n"
+    prompt_text += """
 You: こんにちは、あなたは誰ですか？
 AI: 私はOpenAIによって作られた人工知能です。今日はどうされますか？"""
 
@@ -19,7 +19,7 @@ AI: 私はOpenAIによって作られた人工知能です。今日はどうさ�
             model="text-davinci-003",
             prompt=prompt_text,
             temperature=0.9,
-            max_tokens=150,
+            max_tokens=256,
             top_p=1,
             frequency_penalty=0.0,
             presence_penalty=0.6,
